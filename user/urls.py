4c8d5a2f63cba2ca6493964f9d2ecb75702842cf
samuel <jakeben8990@gmail.com>
2023-12-18 07:59:31 +0100

    creating templates

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("dashboard", views.index, name="dashboard"),
+    path("dashboard/", views.index, name="dashboard"),
+    path("account-details/", views.account_details, name="account_details"),
+    path("statement/", views.statement, name="statement"),
 ]
--- user/views.py
+++ user/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "user/index.html")
+
+
+def account_details(request):
+    return render(request, "user/account-details.html")
+
+
+def statement(request):
+    return render(request, "user/statement.html")
